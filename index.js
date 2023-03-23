ad4caeeb0c35f2b7378b071caf4a868df946746e
Teresia <teresiamwangi26@gmail.com>
2023-03-23 11:42:24 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', addingEventListener);
+    alert('I was clicked!');
+
 }
+addingEventListener();
+
+/*const input = document.getElementById('input');
+
+function addingEventListener() {
+    alert('I was clicked!');
+}
+
+input.addEventListener('click', addingEventListener);*/
+
